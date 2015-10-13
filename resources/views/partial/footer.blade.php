5b5af78b24e5b19290df9e2f2994e14af5930325
klokand <xjz520223@gmail.com>
2015-10-13 15:57:21 +1100

    add footer

--- resources/views/partial/footer.blade.php
+++ resources/views/partial/footer.blade.php
@@ -1,1 +1,51 @@
-this is the footer
+<div id="footer">
+<div class="col-md-2 footer1">
+<ul>
+	<h4>About Us</h4>
+	<li><a href="#">About Us</a></li>
+	<li><a href="#">Design & Construction</a></li>
+	<li><a href="#">Awards</a></li>
+	<li><a href="#">ACompany</a></li>
+	<li><a href="#">Contact Us</a></li>
+	<li><a href="#">Where to Buy</a></li>
+</ul>
+</div>
+<div class="col-md-2 footer2">
+	<ul>
+	<h4>Support</h4>
+	<li><a href="#">FAQ's</a></li>
+	<li><a href="#">Product Help</a></li>
+	<li><a href="#">Parts</a></li>
+	<li><a href="#">Warranty</a></li>
+	<li><a href="#">Return Policy</a></li>
+	<li><a href="#">Shipping and Delivery Policy</a></li>
+	<li><a href="#">CPSIA Product Registration</a></li>
+	</ul>
+</div>
+<div class="col-md-2 footer3">
+<ul>
+	<h4>Community</h4>
+	<li><a href="#">Facebook</a></li>
+	<li><a href="#">Youtube</a></li>
+	<li><a href="#">Blog</a></li>
+	<li><a href="#">WPromotions</a></li>
+</ul>
+</div>
+
+<div class="col-md-2 footer4">
+<ul>
+	<h4>Countries</h4>
+	<li><a href="#">Australia</a></li>
+	<li><a href="#">USA</a></li>
+	<li><a href="#">Japan</a></li>
+	<li><a href="#">Italy</a></li>
+	<li><a href="#">France</a></li>
+	<li><a href="#">UK</a></li>
+	<li><a href="#">Taiwan</a></li>
+</ul>
+</div>
+<div class="col-md-2 footer5">
+<h4 class="col-md-4">Download QuickSmart Brochure</h4>
+<a href="http://www.quicksmartgo.com/index.php?option=com_rubberdoc&amp;view=doc&amp;id=1&amp;format=raw" class="col-md-8"><img src="/img/quicksmart-brochure.jpg" alt=""></a>
+</div>
+</div>
