aa752987086580dcbcb878cfb81853680f0f7f37
klokand <xjz520223@gmail.com>
2015-10-16 17:23:15 +1100

    product detail view main update

--- resources/views/productDetail.blade.php
+++ resources/views/productDetail.blade.php
@@ -6,31 +6,71 @@
 	
 		<div class="product-description col-md-5">
 			<div class="title1">easy fold</div>
-			<div class="title2">Comfort pack</div>
-			<div class="summary">We designed the Comfort Pack just for the QuickSmart Easy Fold Stroller. It ensures five star luxury and comfort for precious little passengers.</div>
+			<div class="title2">Stroller</div>
+			<div class="summary">The QuickSmart™ Easy Fold Stroller is unlike any other
+four wheel stroller. With the revolutionary QuickSmart™
+folding system it is ready to g o in just secon ds!</div>
 			<div class="list">
 				<ul>
-					<li>Seat insert & shoulder pads</li>
-					<li>Fleece-lined for warmth</li>
+					<li>Extremely compact, convenient & lightweight</li>
+					<li>Folds into carry bag, car boot and backseat wells</li>
 					<li>Padded for comfort</li>
-					<li>Machine washable</li>
-					<li>Integrates with harness</li>
+					<li>Adjustable, 2 position seat recline</li>
 				</ul>
 			</div>
 		</div>
 		<div class="product-image col-md-3">
 		<img src="/img/quicksmart-2797_comforter_lr.png" class="img-responsive">
 		</div>
-		<div class="product-buttons col-md-4">
-		this is the buttons
+		<div class="product-rightside col-md-4">
+		 <div class="product-tranbox col-md-12">
+			<div class="product-side1 col-md-12">
+			$383.47 AUD</div>
+			<div class="product-colors col-md-12">
+			<div class="radio">
+				<label>
+					<input type="radio" name="ColorRadios1" id="ColorRadios1" value="Geometric-red-grey">
+					<img src="/img/Geometric-red-grey21.png">
+				</label>
+			</div>
+			<div class="radio">
+				<label>
+					<input type="radio" name="ColorRadios1" id="ColorRadios1" value="Geometric-black-grey">
+					<img src="/img/Geometric-black-grey11.png">
+				</label>
+			</div>
+			</div>
+			<div class="product-actionbuttons col-md-12">
+			<a class="btn btn-success" href="#" role="button" href="#"><i class="fa fa-print fa-2x"></i></a>
+
+			</div>
+		 </div>
+		 <div class="product-viewbuttons col-md-12"></div>
 		</div>
 	 </div>
-	 <div class="product-images col-md-8">
-		<div class="col-md-3"><a class='youtube' href="http://www.youtube.com/embed/LuV_nQnaDDk?rel=0&amp;wmode=transparent"><img class="img-responsive" src="/img/easy-fold-stroller-sizzle.png"></a></div>
-		<div class="col-md-3"><a class="group1" href="/img/easyfold1.jpg" title=""><img class="img-responsive" src="/img/easyfold1.jpg"></a></div>
-		<div class="col-md-3"><a class="group1" href="/img/easy-fold-stroller.jpg" title=""><img class="img-responsive" src="/img/easy-fold-stroller.jpg"></a></div>
+	 <div class="nopadding product-images col-md-8">
+		<div class="col-md-4"><a class='youtube' href="http://www.youtube.com/embed/LuV_nQnaDDk?rel=0&amp;wmode=transparent"><img class="img-responsive" src="/img/easy-fold-stroller-sizzle.png"></a></div>
+		<div class="col-md-4"><a class="group1" href="/img/easyfold1.jpg" title=""><img class="img-responsive" src="/img/QS0719.png"></a></div>
+		<div class="col-md-4"><a class="group1" href="/img/easy-fold-stroller.jpg" title=""><img class="img-responsive" src="/img/Quicksmart-2760.png"></a></div>
 	 </div>
-	 <div class="feedbacks col-md-4">feedback</div>
+	 <div class="feedbacks col-md-4"><p>I get so many jealous looks in the carpark when I open my stroller with one hand, and little Issy in my other arm. She loves it too which makes my life easier.</p><h3>Karinne Mcneill</h3></div>
+	
+	<div>
+
+  <!-- Nav tabs -->
+  <ul class="nav nav-tabs" role="tablist">
+    <li role="presentation" class="active"><a href="#features" aria-controls="features" role="tab" data-toggle="tab">Features</a></li>
+    <li role="presentation"><a href="#specifications" aria-controls="specifications" role="tab" data-toggle="tab">Specifications</a></li>
+  </ul>
+
+  <!-- Tab panes -->
+  <div class="tab-content">
+    <div role="tabpanel" class="tab-pane active" id="features">Features</div>
+    <div role="tabpanel" class="tab-pane" id="specifications">Specifications</div>
+  </div>
+
 	</div>
+</div>
+	
 </div>
 @endsection
