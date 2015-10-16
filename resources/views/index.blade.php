5bc1338a06c7bb9dd80e0dde28757807dbba0fc6
klokand <xjz520223@gmail.com>
2015-10-17 00:22:09 +1100

    home page updated

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,44 +1,45 @@
 @extends('app-front')
 @section('content')
 @include('partial.slider')
-<div id="buttons-section" class="row">
-<div class="col-md-4">
-	<a href="/component/tienda/manufacturers/products?layout=products"><img alt="free-shipping" src="/img/free-shipping.png" class="img-responsive" ></a>
-</div>
-<div class="col-md-4"><a href="/awards"><img src="/img/drtoytile.png" alt="drtoytile" class="img-responsive"></a></div>
-<div class="col-md-4"><a target="_blank" href="http://www.facebook.com/QuickSmartGo"><img class="img-responsive" alt="Connect with us on Facebook" src="/img/connect-facebook.png"></a></div>
+<div id="post-section" class="row">
+<img src="/img/923x185 home page.jpg" class="img-responsive">
 </div>
 <div id="video-action">
 	<h2>View the QuickSmart products in action</h2>
-	<div class="col-md-3">
+	<div class="col-md-4">
 	<a class='youtube' href="http://www.youtube.com/embed/LuV_nQnaDDk?rel=0&amp;wmode=transparent"><img src="/img/easyfold-stroller-video.png"></a><p>Easy Fold Stroller</p>
 	</div>
-	<div class="col-md-3">
+	<div class="col-md-4">
 	<a class='youtube' href="http://www.youtube.com/embed/2-1U7ZbvFZc?rel=0&amp;wmode=transparent"><img src="/img/backpack-stroller-video.png"></a><p>Backpack Stroller</p>
 	</div>
-	<div class="col-md-3">
+	<div class="col-md-4">
 	<a class='youtube' href="http://www.youtube.com/embed/Z7KZcI-okPQ?rel=0&amp;wmode=transparent"><img src="/img/travel-cot-video.png"></a><p>3 in 1 Travel Cot</p>
-	</div>
-	<div class="col-md-3">
-	<a href="#"><img src="/img/easyfold-stroller-video.png"></a><p>Safe Guard Stroller</p>
 	</div>
 </div>
 <div id="products">
 <h2>Product Range</h2>
-	<div class="col-md-2">
-	<a href="#"><img class="img-responsive" src="/img/Geometric Red-t.jpg"></a>
-	</div>
-	<div class="col-md-2">
-	<a href="#"><img class="img-responsive" src="/img/Black-t.jpg"></a>
-	</div>
-	<div class="col-md-2">
+	<div class="item col-md-offset-1 col-md-2">
 	<a href="#"><img class="img-responsive" src="/img/award grey EFS-t.jpg"></a>
 	</div>
-	<div class="col-md-2">
+	<div class="item col-md-2">
 	<a href="#"><img class="img-responsive" src="/img/award red EFS-t.jpg"></a>
 	</div>
-	<div class="col-md-2">
+	<div class="item col-md-2">
 	<a href="#"><img class="img-responsive" src="/img/award travel cot-t.jpg"></a>
 	</div>
+	<div class="item col-md-2">
+	<a href="#"><img class="img-responsive" src="/img/Geometric Red-t.jpg"></a>
+	</div>
+	<div class="item col-md-2">
+	<a href="#"><img class="img-responsive" src="/img/Black-t.jpg"></a>
+	</div>
+</div>
+<div id="links-section" class="row">
+<ul>
+	<li><a href="/about-us"></a></li>
+	<li><a href="/contact-us"></a></li>
+	<li><a target="_blank" href="https://www.youtube.com/user/QuickSmartGo"></a></li>
+	<li><a href="#"></a></li>
+</ul>
 </div>
 @endsection
--- resources/views/partial/header.blade.php
+++ resources/views/partial/header.blade.php
@@ -1,15 +1,17 @@
 <div id="topbar" >
-	<div id="topSocials" class="col-md-offset-4 col-md-8 hidden-print">
-		<a href="http://youtube.com/QuickSmartGo" rel="nofollow" title="YouTube: QuickSmartGo" target="_blank"><img src="/img/youtube.png" alt="YouTube: QuickSmartMove"></a>
-		<a href="/contactus">Contact Us</a>
-	</div>
 	<div id="logo" >
-		<div class="col-md-6"><a href="/" title="QuickSmartMOve"><img class="img-responsive" src="/img/quicksmart.png" alt="QuickSmartMove"></a></div>
-		<div id="logo2" class="col-md-6"><img src="/img/quick-smart-go.png" class="img-responsive"></div>
+		<div id="logo1" class="col-md-3"><a href="/" title="QuickSmartMOve"><img class="img-responsive" src="/img/quicksmart.png" alt="QuickSmartMove"></a></div>
+		<div id="topbar2" class="col-md-offset-1 col-md-8">
+				<div id="topSocials" class="col-md-offset-8 col-md-4 hidden-print">
+				<a href="http://youtube.com/QuickSmartGo" rel="nofollow" title="YouTube: QuickSmartGo" target="_blank"><img class="img-responsive" src="/img/youtube.png" alt="YouTube: QuickSmartMove"></a>
+					<a href="/contact-us">Contact Us</a>
+				</div>
+			<div id="logo2" class="col-md-12"><img src="/img/quick-smart-go.png" class="img-responsive"></div>
+		</div>
 	</div>
 </div>
 <div class="col-md-12">
 		@include('partial.nav')
-	</div>
+</div>
 
 
